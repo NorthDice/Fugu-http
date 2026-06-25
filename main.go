192e6c66e8ba70b338e6e155a6c2588bc0b342bb
NorthDice <mister.balychev@gmail.com>
2026-06-25 18:54:04 +0100

    refactor file reading

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -12,13 +14,41 @@ func main() {
 	if err != nil {
 		log.Fatalf("Cannot open the file, err = %v", err)
 	}
+	defer f.Close()
 
+	buf := make([]byte, 32)
+
+	var lineAccum bytes.Buffer
 	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
+		n, err := f.Read(buf)
+		if n > 0 {
+			currentData := buf[:n]
+
+			for {
+				idx := bytes.IndexByte(currentData, '\n')
+				if idx == -1 {
+					lineAccum.Write(currentData)
+					break
+				}
+
+				lineAccum.Write(currentData[:idx])
+				fmt.Printf("read: %s\n", lineAccum.String())
+
+				lineAccum.Reset()
+
+				currentData = currentData[idx+1:]
+			}
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("Read error: %v", err)
+		}
+
 	}
+	if lineAccum.Len() > 0 {
+		fmt.Printf("read: %s\n", lineAccum.String())
+	}
+
 }
