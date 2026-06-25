e398256a0be13650638f8077d0a0cab28b2dad60
NorthDice <mister.balychev@gmail.com>
2026-06-25 19:27:31 +0100

    refactor main with channels

--- main.go
+++ main.go
@@ -8,6 +8,50 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		defer f.Close()
+		buf := make([]byte, 16)
+
+		var lineAccum bytes.Buffer
+		for {
+			n, err := f.Read(buf)
+			if n > 0 {
+				currentData := buf[:n]
+
+				for {
+					idx := bytes.IndexByte(currentData, '\n')
+					if idx == -1 {
+						lineAccum.Write(currentData)
+						break
+					}
+
+					lineAccum.Write(currentData[:idx])
+					out <- lineAccum.String()
+					lineAccum.Reset()
+
+					currentData = currentData[idx+1:]
+				}
+			}
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatalf("Read error: %v", err)
+			}
+
+		}
+		if lineAccum.Len() > 0 {
+			out <- lineAccum.String()
+		}
+	}()
+
+	return out
+}
+
 func main() {
 
 	f, err := os.Open("messages.txt")
@@ -16,39 +60,8 @@ func main() {
 	}
 	defer f.Close()
 
-	buf := make([]byte, 32)
-
-	var lineAccum bytes.Buffer
-	for {
-		n, err := f.Read(buf)
-		if n > 0 {
-			currentData := buf[:n]
-
-			for {
-				idx := bytes.IndexByte(currentData, '\n')
-				if idx == -1 {
-					lineAccum.Write(currentData)
-					break
-				}
-
-				lineAccum.Write(currentData[:idx])
-				fmt.Printf("read: %s\n", lineAccum.String())
-
-				lineAccum.Reset()
-
-				currentData = currentData[idx+1:]
-			}
-		}
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatalf("Read error: %v", err)
-		}
-
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-	if lineAccum.Len() > 0 {
-		fmt.Printf("read: %s\n", lineAccum.String())
-	}
-
 }
