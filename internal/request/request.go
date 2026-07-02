674dbe4c8e30456a2d13830dbe01f614712ee04a
Kyryl Balychev <kyoto@Kyryls-MacBook-Air.local>
2026-07-02 21:19:57 +0100

    implemented reading of request

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"errors"
+	"fmt"
 	"fugu-http/internal/domain"
 	"io"
 	"strings"
@@ -11,11 +13,6 @@ type RequestLine struct {
 	RequestTarget string
 	Method        string
 }
-
-func (r *RequestLine) ValidHTTP() bool {
-	return r.HttpVersion == "HTTP/1.1"
-}
-
 type Request struct {
 	RequestLine RequestLine
 }
@@ -37,14 +34,37 @@ func parseRequestLine(b string) (*RequestLine, string, error) {
 	if len(parts) != 3 {
 		return nil, restOfMessage, domain.MALFORMED_REQUEST_LINE
 	}
-	rl := RequestLine{
+
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfMessage, domain.MALFORMED_REQUEST_LINE
+	}
+
+	rl := &RequestLine{
 		Method:        parts[0],
 		RequestTarget: parts[1],
-		HttpVersion:   parts[2],
-	}, restOfMessage, nil
+		HttpVersion:   httpParts[1],
+	}
 
+	return rl, restOfMessage, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(
+			fmt.Errorf("unable to read io.ReadAll"),
+			err,
+		)
+	}
 
+	str := string(data)
+	rl, _, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: *rl,
+	}, err
 }
